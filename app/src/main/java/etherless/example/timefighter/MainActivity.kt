07581d7f5290316968c8d71dee6f4d8c8881e148
Pablo Fernandez Guerra <pabloimrik17@gmail.com>
2019-03-31 14:22:48 +0200

    Added variables to handle UI elements on the view

--- app/src/main/java/etherless/example/timefighter/MainActivity.kt
+++ app/src/main/java/etherless/example/timefighter/MainActivity.kt
@@ -2,11 +2,20 @@ package etherless.example.timefighter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    internal lateinit var tapMeButton: Button
+    internal lateinit var gameScoreTextView: TextView
+    internal lateinit var timeLeftTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tapMeButton = findViewById<Button>(R.id.tapMeButton)
+        gameScoreTextView = findViewById<TextView>(R.id.gameScoreTextView)
+        timeLeftTextView = findViewById<TextView>(R.id.timeLeftTextView)
     }
 }

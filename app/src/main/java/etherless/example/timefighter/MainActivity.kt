df2705b4be971276b494176c59df46fc8bd3debf
Pablo Fernandez Guerra <pabloimrik17@gmail.com>
2019-04-04 20:23:33 +0200

    Initialized score text on the view.
    Added listener to the tap me button to increase the score.

--- app/src/main/java/etherless/example/timefighter/MainActivity.kt
+++ app/src/main/java/etherless/example/timefighter/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     internal lateinit var tapMeButton: Button
     internal lateinit var gameScoreTextView: TextView
     internal lateinit var timeLeftTextView: TextView
+    internal var score: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +18,21 @@ class MainActivity : AppCompatActivity() {
         tapMeButton = findViewById<Button>(R.id.tapMeButton)
         gameScoreTextView = findViewById<TextView>(R.id.gameScoreTextView)
         timeLeftTextView = findViewById<TextView>(R.id.timeLeftTextView)
+        this.setScore()
+
+        tapMeButton.setOnClickListener { view ->
+            incrementScore()
+        }
+    }
+
+    private fun setScore() {
+        val newScore: String = getString(R.string.yourScore, score.toString())
+
+        this.gameScoreTextView.text = newScore
+    }
+
+    private fun incrementScore() {
+        this.score += 1;
+        this.setScore()
     }
 }
